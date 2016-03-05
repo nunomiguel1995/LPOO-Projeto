b38a485c68ccc32d99e74e4036afe5e2cc2752f1
Nuno Miguel <nuno.migcastro@gmail.com>
2016-03-05 22:23:13 +0000

    Mais testes

--- src/maze/test/TestLabirintoDragaoDinamico.java
+++ src/maze/test/TestLabirintoDragaoDinamico.java
@@ -11,5 +11,27 @@ public class TestLabirintoDragaoDinamico {
 					{'X',' ',' ','D','X'},
 					{'X','X','X','X','X'}};
 	
-	boolean dragaoParado = false, dragaoAdormece = true;
+	@Test
+	public void testDragaoDorme(){
+		Jogo j = new Jogo(lab);
+		j.lePosicoes();
+		j.getDragao().adormeceOuAcorda();
+		if(j.getDragao().getSimbolo() == 'D'){
+			assertFalse(j.getDragao().getAdormecido());
+		}else{
+			assertTrue(j.getDragao().getAdormecido());
+		}
+	}
+	
+	@Test
+	public void testDragaoCimaEspada(){
+		Jogo j = new Jogo(lab);
+		j.lePosicoes();
+		j.getDragao().setPosicao(j.getEspada().getPosicao());
+		j.getDragao().dragaoCimaEspada(j.getEspada());
+		assertTrue(j.getDragao().getCimaEspada());
+		if(j.getDragao().getSimbolo() == 'F'){
+			assertTrue(j.getDragao().getCimaEspada());
+		}
+	}
 }
