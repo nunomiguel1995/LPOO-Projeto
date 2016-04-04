3e71e6973716506991cfaccbdc6008e39f6f0297
Nuno <nuno.migcastro@gmail.com>
2016-04-04 20:05:04 +0100

    no message

--- src/maze/cli/main.java
+++ src/maze/cli/main.java
@@ -1,4 +1,5 @@
 package maze.cli;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import maze.logic.*;
@@ -10,18 +11,27 @@ public class main {
 	private static Jogo j;
 	
 	public static void main(String[] args) {
-		initializeGame();
-		
+		try{
+			initializeGame();
+		}catch(InputMismatchException ex){
+			System.out.println("Valores Invalidos! Insira apenas números!");
+			return;
+		}catch(IllegalArgumentException e){
+			System.out.println("Valores Invalidos! Altura e largura-> numeros maiores que 3 e impares;\n"
+					+ "numero de dragoes->maior que 0");
+			return;
+		}
 		while(j.getEstado() != EstadoJogo.PERDEU && j.getEstado() != EstadoJogo.GANHOU){
 			System.out.print(j.toString());
-			iteration();
+			
+				iteration();
 		}		
 		s.close();		
 	}
-	
-	public static void initializeGame(){
+
+	public static void initializeGame() throws IllegalArgumentException, InputMismatchException{
 		int altura,largura, nDragoes, opcao;
-		try{		
+
 		System.out.println("Escolha as dimensoes do labirinto:");
 		System.out.print("Altura: ");
 		altura=s.nextInt();
@@ -29,29 +39,24 @@ public class main {
 		largura=s.nextInt();
 		System.out.print("Numero de Dragoes a enfrentar: ");
 		nDragoes=s.nextInt();
-		
+
 		if(altura<= 3|| largura <= 3 || nDragoes <= 0 
-							||(altura % 2) == 0 || (largura %2) == 0)
+				||(altura % 2) == 0 || (largura %2) == 0)
 			throw new IllegalArgumentException();
-		
+
 		j = new Jogo(altura,largura,nDragoes);
- 		
+
 		System.out.println("Qual a estrategia pretendida (selecione a opcao):");
 		System.out.println("1 - Dragao parado");
 		System.out.println("2 - Dragao com movimentacao aleatoria");
 		System.out.println("3 - Dragão com movimentacao aleatoria intercalada com dormir");
-		
+
 		opcao = s.nextInt();
 		if(opcao!=1 && opcao!=2 && opcao!=3)
 			throw new IllegalArgumentException();
 		j.setComportamentoDragao(opcao);
-
-		}catch(IllegalArgumentException e){
-			System.out.println("Valores Invalidos!");
-		}
-		
 	}
-	
+
 	public static void iteration(){
 		int direcao;
 		System.out.println("1 - Cima");
@@ -59,7 +64,13 @@ public class main {
 		System.out.println("3 - Esquerda");
 		System.out.println("4 - Direita");
 		System.out.print("Escolha a direcao: ");	
-		direcao = s.nextInt();
+		try{
+			direcao = s.nextInt();
+		}catch(InputMismatchException e){
+			System.out.println("Opcao invalida!");
+			s.nextLine();
+			return;
+		}
 		System.out.println();
 		switch(direcao){
 		case 1: j.jogada(Direcao.CIMA); break;
